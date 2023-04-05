ff89d5a75de9a9c624fa05f2271081bceef0653b
Ignacio Peralta <112433497+ElOtroNach0@users.noreply.github.com>
2023-04-04 22:36:12 -0300

    Update scrip.sql

--- scrip.sql
+++ scrip.sql
@@ -4,37 +4,44 @@ CREATE TABLE pax (
   last_name VARCHAR(50) NOT NULL,
   documentacion VARCHAR(50) NOT NULL,
   fecha_nacimiento DATE NOT NULL,
-  sexo VARCHAR(10) NOT NULL,
+  sexo VARCHAR(10) NOT NULL
 );
 
 CREATE TABLE tipo_servicio (
   id_reserva INT PRIMARY KEY AUTO_INCREMENT,
-  id_pax id_metodo_pago INT NOT NULL,
-fecha_compra DATE NOT NULL,
-id_servicio INT NOT NULL,
-id_facturacion INT NOT NULL,
+  id_pax INT NOT NULL,
+  id_metodo_pago INT NOT NULL,
+  fecha_compra DATE NOT NULL,
+  id_servicio INT NOT NULL,
+  id_facturacion INT NOT NULL,
+  FOREIGN KEY (id_pax) REFERENCES pax(id_pasajero),
+  FOREIGN KEY (id_metodo_pago) REFERENCES metodo_pago(id_metodo_pago),
+  FOREIGN KEY (id_servicio) REFERENCES tipo_servicio(id_servicio),
+  FOREIGN KEY (id_facturacion) REFERENCES factura(id_facturacion)
 );
 
 CREATE TABLE tipo_servicio (
   id_servicio INT PRIMARY KEY AUTO_INCREMENT,
-  nombre_servicio VARCHAR(50) NOT NULL,
+  nombre_servicio VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE factura (
   id_facturacion INT PRIMARY KEY AUTO_INCREMENT,
   id_metodo_pago INT NOT NULL,
   monto_total DECIMAL(10,2) NOT NULL,
-  id_tipo_factura VARCHAR(50) NOT NULL
-id_servicio VARCHAR(255) NOT NULL,	
+  id_tipo_factura VARCHAR(50) NOT NULL,
+  id_servicio INT NOT NULL,
+  FOREIGN KEY (id_metodo_pago) REFERENCES metodo_pago(id_metodo_pago),
+  FOREIGN KEY (id_servicio) REFERENCES tipo_servicio(id_servicio)
 );
 
 CREATE TABLE metodo_pago (
   id_metodo_pago INT PRIMARY KEY AUTO_INCREMENT,
-  nombre VARCHAR(50) NOT NULL,
+  nombre VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE TipoFactura (
   id_tipo_factura INT PRIMARY KEY AUTO_INCREMENT,
   tipo_factura VARCHAR(2) NOT NULL,
-tipo_consumidor VARCHAR(50),
+  tipo_consumidor VARCHAR(50)
 );
